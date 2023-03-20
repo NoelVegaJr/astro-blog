b8aa43f085f9ca8a88239d759fb16ce647a3d187
NoelVegaJr <94807574+NoelVegaJr@users.noreply.github.com>
2023-03-20 01:46:14 -0400

    tailwind install successfully (#9)

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -9,7 +9,7 @@ export default function Greeting({ messages }: { messages: any }) {
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
+      <h3 className="line-through">{greeting}! Thank you for visiting!</h3>
       <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
     </div>
   );
